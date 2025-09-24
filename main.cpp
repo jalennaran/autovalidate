3c255d794f54c593c83b56dcf58da0e6d3a732e3
jalennaran <jalen.naran@gmail.com>
2025-09-24 11:21:14 -0700

    fixed merge conflicts between main and fix1, closes #1

--- main.cpp
+++ main.cpp
@@ -2,32 +2,36 @@
 #include <cstdlib>
 #include <ctime>
 #include <vector>
+#include <cctype>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 using std::vector;
+using std::transform;
 
-const vector <string> VALIDATION = {"Cool","Great","Perfect","Beautiful"};
+const vector <string> VALIDATION = {"Cool","Great","Perfect","Beautiful","Aw, yeah"};
 
 int main(){
   string input;
   int pick;
 
   srand(time(0));
-  pick = rand() % 4;
+  pick = rand() % VALIDATION.size();
   cout << "What are you listening to?\n";
   getline(cin,input);
   if(input == "nothing"){
     return 0;
   }
+  transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
   cout << VALIDATION[pick] << "! Let's listen to more\n";
 
   do{
     cout << "What's next?\n";
     getline(cin,input);
-    pick = rand() % 4;
+    transform(input.begin(), input.end(), input.begin(), [](unsigned char c){ return std::tolower(c); });
+    pick = rand() % VALIDATION.size();
     cout << VALIDATION[pick] << "!\n";
   }while( input != "nothing" );
 
